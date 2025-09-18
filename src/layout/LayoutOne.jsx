087b4d82ff46d45e3600ed7fb6e11f833d1355f5
Antor Web-Dev <antorremeni@gmail.com>
2025-09-18 11:27:26 +0600

    chnage

---
+++ src/layout/LayoutOne.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
+
+const LayoutOne = () => {
+  return (
+    <>
+    
+    
+    <Navbar />
+    <Outlet />
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default LayoutOne
