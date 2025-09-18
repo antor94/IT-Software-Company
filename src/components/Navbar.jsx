a9e2e69e99bce1658b1e17e01c4a9898dce899ff
Antor Web-Dev <antorremeni@gmail.com>
2025-09-18 11:30:23 +0600

    chnage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import logo from '../assets/images/solid.svg'
+import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    
+    <>
+
+
+    <nav id='navbar'>
+        <div className="container">
+            <div id='navbar-row'>
+
+                <Link to={'/'} ><img src={logo} alt="main-logo" /></Link>
+
+
+
+            </div>
+        </div>
+    </nav>
+    
+
+
+    </>
+
   )
 }
 
--- src/layout/LayoutOne.jsx
+++ src/layout/LayoutOne.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const LayoutOne = () => {
   return (
@@ -9,7 +10,7 @@ const LayoutOne = () => {
     
     <Navbar />
     <Outlet />
-    
+    <Footer />
     
     
     
