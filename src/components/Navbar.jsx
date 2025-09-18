ceb31c44af6ffbb814c46d0df5120a862518f06a
Antor Web-Dev <antorremeni@gmail.com>
2025-09-18 11:32:57 +0600

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../assets/images/solid.svg'
 import { Link } from 'react-router'
+import { IoToggle } from "react-icons/io5";
+
 
 const Navbar = () => {
   return (
@@ -11,9 +13,19 @@ const Navbar = () => {
     <nav id='navbar'>
         <div className="container">
             <div id='navbar-row'>
-
+{/* ---------- main-logo */}
                 <Link to={'/'} ><img src={logo} alt="main-logo" /></Link>
-
+{/* ------------ nav-items */}
+<div>
+    <ul>
+        <li><Link to={'/'} >Services</Link></li>
+        <li><Link to={'/'} >Case Studies</Link></li>
+        <li><Link to={'/'} >About</Link></li>
+        <li><Link to={'/'} >News</Link></li>
+        <li><Link to={'/'} >Contact</Link></li>
+    </ul>
+</div>
+{/* ---------- toggle button */}
 
 
             </div>
