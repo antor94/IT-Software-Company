06033c6689d4f6624680eb34a34501f691372821
Antor Web-Dev <antorremeni@gmail.com>
2025-09-19 12:05:48 +0600

    chnage

--- src/components/common/NewsCommon.jsx
+++ src/components/common/NewsCommon.jsx
@@ -10,6 +10,11 @@ const NewsCommon = () => {
     <div className='w-[387px] h-[386px] bg-white shadow-[0px_4px_20px_rgba(0,0,0,0.1)] rounded-[8px] '>
         <div className='p-[40px]'>
 
+            <div>
+                <p className='w-[75px] px-[8px] bg-[#EFF2FC] text-[14px] font-semibold font-main text-[#3E4265]'>Strategy</p>
+                <p className='text-[14px] font-normal font-main text-[#9397AD]'>12 hours ago</p>
+            </div>
+
 
             <div><h3 className='text-[24px] font-extrabold font-main text-[#3E4265]'>Mobile app generates data for the energy management</h3></div>
             <div className='w-[307px]'><p className='text-[16px] font-normal font-main text-[#585C7B]'>Sociis sit risus id. Sit facilisis dolor arcu, fermentum vestibulum arcu ele. Pulvinar at maecenas maecenas pharetra, tincidunt sollicitudin in in. </p></div>
