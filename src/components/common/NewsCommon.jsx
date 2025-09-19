b3d99131db83ea1daa55e5409489301a2a652e3d
Antor Web-Dev <antorremeni@gmail.com>
2025-09-19 12:14:58 +0600

    chnage

--- src/components/common/NewsCommon.jsx
+++ src/components/common/NewsCommon.jsx
@@ -3,21 +3,21 @@ import { AiOutlineLike } from "react-icons/ai";
 import { VscComment } from "react-icons/vsc";
 import { LuShare2 } from "react-icons/lu";
 
-const NewsCommon = () => {
+const NewsCommon = ({newsP , newsHead , newsDis , newsT}) => {
   return (
     <>
 
-    <div className='w-[387px] h-[386px] group hover:bg-[#6366F1]  hover:text-white bg-white shadow-[0px_4px_20px_rgba(0,0,0,0.1)] rounded-[8px] '>
+    <div className='w-[387px] h-[386px] group hover:bg-[#6366F1] duration-[.4s]  hover:text-white bg-white shadow-[0px_4px_20px_rgba(0,0,0,0.1)] rounded-[8px] '>
         <div className='p-[40px] '>
 
             <div className='flex'>
-                <p className='w-[75px] px-[8px] bg-[#EFF2FC] text-[14px] font-semibold font-main text-[#3E4265]'>Strategy</p>
+                <p className='w-[75px] px-[8px] bg-[#EFF2FC] text-[14px] font-semibold font-main text-[#3E4265]'>{newsP}</p>
                 <span className=' inline-block w-[1px] h-[20px] mx-[12px] bg-[#E2E5F1]'></span>
                 <p className='text-[14px] font-normal font-main group-hover:text-white text-[#9397AD]'>12 hours ago</p>
             </div>
 
 
-            <div className='my-[16px] '><h3 className='text-[24px] group-hover:text-white font-extrabold font-main text-[#3E4265]'>Mobile app generates data for the energy management</h3></div>
+            <div className='my-[16px] '><h3 className='text-[24px] group-hover:text-white font-extrabold font-main text-[#3E4265]'>{newsHead}</h3></div>
             <div className='w-[307px] mb-[24px]'><p className='text-[16px] font-normal group-hover:text-white font-main text-[#585C7B]'>Sociis sit risus id. Sit facilisis dolor arcu, fermentum vestibulum arcu ele. Pulvinar at maecenas maecenas pharetra, tincidunt sollicitudin in in. </p></div>
 
             {/* -------- icon */}
