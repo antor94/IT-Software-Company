a7f6ca36aec6523aa0591bed93652248c3d06b5c
Antor Web-Dev <antorremeni@gmail.com>
2025-09-21 20:31:20 +0600

    chnage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -17,19 +17,23 @@ const Banner = () => {
           backgroundRepeat: `no-repeat`,
           backgroundSize: `cover`,
         }}
-        className=" bg-left lg:backgroundPosition: center, w-full pt-[163px] pb-[199px]  relative z-999 lg:py-[228px]"
-      >
-        <div className=" absolute top-0 left-0 bg-linear-to-r from-[#13102260] to-[#13102298] w-full h-100"></div>
-
+        className=" bg-left lg:backgroundPosition: center, w-full pt-[163px] pb-[199px]  relative z-999 lg:py-[228px]" >
         <div className="container">
-          <div id="banner-row" className=" z-999">
-            <div id="heading" className="w-[856px] z-9999">
-              <div className="w-[856px] text-center">
+          <div id="banner-row" className=" ">
+            {/* ------------- heading */}
+            <div id="heading" className="   pt-[207px] pb-[143px]">
+
+
+                <div className="w-[856px] text-center">
+
                 <h1 className="text-[72px] z-9999 font-extrabold font-main text-white">
                   The Best IT Solutions for Your Business
                 </h1>
-              </div>
-              <div className="w-[856px] text-center">
+
+                </div>
+
+        
+              <div className=" mt-[24px] mb-[56px] text-center">
                 <p className="text-[20px] font-normal font-main text-[#fff]">
                   We carry out the widest range of high quality IT services and
                   solutions to our customers making their business better.
@@ -40,6 +44,8 @@ const Banner = () => {
               </button>
             </div>
             {/* ----------- icons */}
+            <div className=" pb-[96px] flex justify-around items-center">
+
             <div className=" flex items-center gap-[8px] ">
               <div className="w-[44px] h-[44px] bg-[#ffffff1e] shadow-[20px_20px_60px_-15px_rgba(255,_255,_255,_0.5)] group  hover:bg-white duration-[.3s] rounded-full flex justify-center items-center">
                 <Link to={"/"}>
@@ -65,7 +71,9 @@ const Banner = () => {
                 </Link>
               </div>
             </div>
-            <button className="w-[164px] text-[16px] font-semibold font-main text-[#fff]">Our case studies <FaRegArrowAltCircleRight /></button>
+            <button className="w-[164px] text-[16px] font-semibold font-main flex items-center gap-[8px] text-[#fff]">Our case studies <FaRegArrowAltCircleRight /></button>
+            </div>
+
           </div>
         </div>
       </section>
---
+++ src/components/Case.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Case = () => {
+  return (
+    <>
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Case
