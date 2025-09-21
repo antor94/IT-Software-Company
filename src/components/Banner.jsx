5cbc38d05e8237dd169c6269a2958a68ce343d15
Antor Web-Dev <antorremeni@gmail.com>
2025-09-21 20:00:33 +0600

    chnage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,10 +7,14 @@ const Banner = () => {
     <>
     
     
-    <section id='banner'>
+    <section  id='banner' style={{  background: `URL(${bannerImg})`,  backgroundRepeat: `no-repeat`,    backgroundSize: `cover`, }}
+        className=" bg-left lg:backgroundPosition: center, w-full pt-[163px] pb-[199px] relative lg:py-[228px]" > 
+        <div className='w-full h-[600px] bg-[#131022] absolute top-0 left-0 opacity-50 '></div>
         <div className="container">
             <div id='banner-row'>
+                <div className=''>
 
+                </div>
             </div>
         </div>
     </section>
